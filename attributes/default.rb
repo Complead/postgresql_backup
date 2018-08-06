e58dbb27c37f31dcc3ee499ad92f7685734f28ee
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-08-06 13:38:32 +0300

    add region parameter

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['backup']['aws']['keep'] = 360
 default['backup']['major_version'] = 4
+
+default['backup']['aws']['region'] = 'eu-central-1'
+default['backup']['aws']['keep'] = 360
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@ backup_lwrp_generate_model node.name do
     "settings" => {
       "s3.access_key_id" => "#{node['backup']['aws']['access_key_id']}",
       "s3.secret_access_key" => "#{node['backup']['aws']['secret_access_key']}",
-      "s3.region" => "eu-central-1",
+      "s3.region" => "#{node['backup']['aws']['region']}",
       "s3.bucket" => "#{node['backup']['aws']['bucket']}",
       "s3.path" => "/",
       "s3.keep" => node['backup']['aws']['keep']
